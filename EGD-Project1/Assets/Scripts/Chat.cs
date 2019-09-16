e8b8af245ae3d11c3f12260a977259fadf254a93
Francys Dong <francysdong@gmail.com>
2019-09-16 01:11:17 -0400

    WIP instantiate chatboxes

---
+++ EGD-Project1/Assets/Scripts/Chat.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ChatText
+{
+    public string content;
+    public bool needSelection;
+    public List<string> choices;
+}
+
+public class Chat : MonoBehaviour
+{
+    public List<ChatText> texts;
+
+    public int index;
+    public GameObject girlBox;
+    public GameObject myBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        CreateChatBox(true, "nya");
+        CreateChatBox(false, "okie dokie andhjhjhjhjhjhj.");
+        //ChatStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChatStart()
+    {
+        bool checkTurn = false;
+        for (index = 0; index < texts.Count -1; index++)
+        {
+            checkTurn = texts[index].needSelection;
+            if (checkTurn)
+            {
+                //next doing choices have to sleep
+            }
+        }
+    }
+
+    public void CreateChatBox(bool myTurn, string text)
+    {
+        GameObject go;
+        
+        if (myTurn)
+        {
+            go = Instantiate(myBox);
+            
+        }
+        else
+        {
+            go = Instantiate(girlBox);
+            
+        }
+        go.transform.parent = GameObject.Find("ChatContent").transform;
+        go.GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        OverflowChat overScript = go.GetComponentInChildren<OverflowChat>();
+        overScript.SetContent(text);
+        overScript.AdjustWidth();
+        
+
+    }
+}
--- EGD-Project1/Assets/Scripts/OverflowChat.cs
+++ EGD-Project1/Assets/Scripts/OverflowChat.cs
@@ -6,35 +6,43 @@ using UnityEngine;
 public class OverflowChat : MonoBehaviour
 {
     public bool rightOrient;
+    //public GameObject Box; 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-        int textLength = CalculateLengthOfMessage();
-        int leftPadding = (int)(0.00017 * textLength * textLength - 0.1662 * textLength - 8);
-        int rightPadding = (int)(-0.0469 * textLength - 11);
 
-        this.GetComponent<RectTransform>().sizeDelta = new Vector2(textLength, 100);
+        AdjustWidth();
+        print("hjhj");
         //this.GetComponent<HorizontalLayoutGroup>().padding.left = leftPadding;
         //this.GetComponent<HorizontalLayoutGroup>().padding.right = rightPadding;
-        if (rightOrient)
-        {
-            this.GetComponent<RectTransform>().anchoredPosition = new Vector3(200 - textLength / 2 + rightPadding, 1, 0);
-            this.GetComponent<HorizontalLayoutGroup>().padding.right = leftPadding;
-            this.GetComponent<HorizontalLayoutGroup>().padding.left = rightPadding;
-        }
-        else
-        {
-            this.GetComponent<RectTransform>().anchoredPosition = new Vector3(-213 + textLength / 2 - leftPadding, 1, 0);
-            this.GetComponent<HorizontalLayoutGroup>().padding.left = leftPadding;
-            this.GetComponent<HorizontalLayoutGroup>().padding.right = rightPadding;
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AdjustWidth()
+    {
+        int textLength = CalculateLengthOfMessage();
+        int leftPadding = (int)(0.00017 * textLength * textLength - 0.1662 * textLength - 8);
+        int rightPadding = (int)(-0.0469 * textLength - 11);
+
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(textLength, 100);
+        if (rightOrient)
+        {
+            this.GetComponent<RectTransform>().anchoredPosition = new Vector3(193 - textLength / 2 + rightPadding, 1, 0);
+            this.GetComponent<HorizontalLayoutGroup>().padding.right = leftPadding;
+            this.GetComponent<HorizontalLayoutGroup>().padding.left = rightPadding;
+        }
+        else
+        {
+            this.GetComponent<RectTransform>().anchoredPosition = new Vector3(-209 + textLength / 2 - leftPadding, 1, 0);
+            this.GetComponent<HorizontalLayoutGroup>().padding.left = leftPadding;
+            this.GetComponent<HorizontalLayoutGroup>().padding.right = rightPadding;
+        }
     }
 
     private int CalculateLengthOfMessage()
@@ -55,5 +63,12 @@ public class OverflowChat : MonoBehaviour
         }
 
         return totalLength;
+    }
+
+    public void SetContent(string blah)
+    {
+        Text chatText = this.GetComponent<Text>();
+        chatText.text = blah;
+        AdjustWidth();
     }
 }
