a79c5dd8bf9b5b0ca19c4338097c9cb0157cd372
Miguel <miggels97@gmail.com>
2019-09-18 13:42:00 -0400

    Hid mouse

--- EGD-Project1/Assets/Scripts/Clicker.cs
+++ EGD-Project1/Assets/Scripts/Clicker.cs
@@ -13,7 +13,7 @@ public class Clicker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
